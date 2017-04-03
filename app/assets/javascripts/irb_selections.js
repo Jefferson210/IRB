2f7feabf6a4eb32330177822d9d964d2473a33c6
Alejandro <alejosanzcr7@gmail.com>
2017-04-03 00:44:44 +0000

    1.Validación En Modulo Crossings referente a cruce en relación al número de repeticiones. 2.Creación del Código IRB en Modulo irb_selections

--- app/assets/javascripts/irb_selections.js
+++ app/assets/javascripts/irb_selections.js
@@ -32,4 +32,85 @@ $( document ).on('turbolinks:load', function() {
             $("#colorId").val("");
         }
     });
+    
+    
+    
+    array = [
+           "2012|A",
+           "2013|B",
+           "2014|C",
+           "2015|D",
+           "2016|E",
+           "2017|F",
+           "2018|G",
+           "2019|H",
+           "2020|I",
+           "2021|J",
+           "2022|K",
+           "2023|L",
+           "2024|M",
+           "2025|N",
+           "2026|O",
+           "2027|P",
+           "2028|Q",
+           "2029|R",
+           "2030|S",
+           "2031|T",
+           "2032|U",
+           "2033|V",
+           "2034|W",
+           "2035|X",
+           "2036|Y",
+           "2037|Z"
+]
+
+function ConvertToYear(letter) {
+
+    for (var item in array) {
+
+        El_Searched = array[item].split('|');
+        yearSearched = El_Searched[0];
+        letterSearched = El_Searched[1];
+
+        if (letter == letterSearched) {
+
+            return yearSearched
+            break;
+
+        }
+
+    }
+
+}
+
+$("#three_offspring_id").change(function () {
+
+    threeOffspring = $("#three_offspring_id option:selected").html();
+
+
+    if (threeOffspring != "Select") {
+        arraythreeOffspring = threeOffspring.split('-');
+        letter = arraythreeOffspring[0].substring(0, 1);
+        cross = arraythreeOffspring[0].substring(1, arraythreeOffspring[0].length);
+        individual = arraythreeOffspring[1];
+
+        year = ConvertToYear(letter).substring(2, 4);
+
+
+        cod = "IRB-" + year + "-" + cross + "-" + individual
+
+        $("#irb_selection_code").val(cod);
+
+    }
+    else {
+
+
+        $("#irb_selection_code").val("");
+    }
+})
+
+    
+    
+
+
 })
